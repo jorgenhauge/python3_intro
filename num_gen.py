5cd060ba73de6167e1734ac2fea160f011ee198a
Jørgen Hauge <johauge@cisco.com>
2016-10-17 16:25:39 +0200

    Adding number generator

---
+++ num_gen.py
@@ -0,0 +1,46 @@
+import sys
+import random
+
+numdb = []
+newnumdb = []
+
+with open('numgen.dat') as f:
+    numdb = [line.strip() for line in f]
+
+
+def generate_number(ndigits=8):
+    return ''.join([random.choice('0123456789') for _ in range(ndigits)])
+
+
+def add_number(number):
+    with open('numgen.dat', 'a') as f:
+        print(number, file=f)
+
+
+def check_and_add(num):
+    global numdb, newnumdb
+    print('checking', num, end=': ')
+    if num in numdb + newnumdb:
+        print('number already exists, not added')
+        return False
+    elif any(num.startswith(n) for n in numdb + newnumdb):
+        print('an existing number is a prefix, not added')
+        return False
+    elif any(n.startswith(num) for n in numdb + newnumdb):
+        print('this is a prefix for an existing number, not added')
+        return False
+    else:
+        print('New number added')
+        newnumdb.append(num)
+        return True
+
+
+if len(sys.argv) != 2:
+    sys.exit('Usage: num_gen.py [n]')
+elif len(sys.argv) == 2:
+    count = 0
+    while count <= int(sys.argv[1]):
+        numb = generate_number()
+        if check_and_add(numb):
+            count += 1
+            add_number(numb)
